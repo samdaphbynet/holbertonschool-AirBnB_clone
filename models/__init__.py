6c90c544c49ea5649e67c84b06f79df8d59dddb8
samdaphbynet <lj8@hotmail.fr>
2023-08-15 23:08:04 +0200

    update unit file

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """commented module"""
-from models.engine.file_storage import FileStorage
+from os import getenv
 
 
-storage = FileStorage()
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
